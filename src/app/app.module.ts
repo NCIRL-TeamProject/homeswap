cd451e5093b6802b29eb04243e93f0d8f6ca7c96
Maxi <herreramaxi@gmail.com>
2021-03-02 00:15:58 +0000

    adding bootstrap module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClickCounterComponent } from './click-counter/click-counter.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ClickCounterComponent } from './click-counter/click-counter.component';
     ClickCounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
