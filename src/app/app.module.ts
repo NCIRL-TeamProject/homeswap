f2049e6494fb131ce3f866d5998a957a8dae99e4
ReneeEscalada <sharmaineescalada@gmail.com>
2021-03-07 17:48:52 +0000

    adding how it works component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ClickCounterComponent } from './click-counter/click-counter.component';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClickCounterComponent
+    ClickCounterComponent,
+    HowItWorksComponent
   ],
   imports: [
     BrowserModule,
