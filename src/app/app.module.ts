d830f1a104e328508663123afcdf9622b196cf4d
Maxi <herreramaxi@gmail.com>
2021-03-07 23:49:52 +0000

    adding home-profile and how-it-works to routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ClickCounterComponent } from './click-counter/click-counter.component';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { HomeProfileComponent } from './home-profile/home-profile.component';
 
 import { RegistrationComponent } from './registration/registration.component';
 
@@ -13,18 +11,19 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HowItWorksComponent } from './home/how-it-works/how-it-works.component';
+import { HomeProfileComponent } from './home-profile/home-profile.component';
 // Shared
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClickCounterComponent,
     RegistrationComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    HowItWorksComponent
+    HomeProfileComponent,
+    HowItWorksComponent    
   ],
   imports: [
     BrowserModule,
