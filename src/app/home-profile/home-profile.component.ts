41e20390467fa38b35586722ffb93d0fceb30ec5
Maxi <herreramaxi@gmail.com>
2021-03-11 23:27:07 +0000

    adding ng form to homeprofile. Adding BaseUrlInterceptor to add baseurl depending on environment.

--- src/app/home-profile/home-profile.component.ts
+++ src/app/home-profile/home-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home-profile',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-profile.component.css']
 })
 export class HomeProfileComponent implements OnInit {
-
-  constructor() { }
+  // homeProfileForm = new FormGroup({
+  //   title: new FormControl(''),
+  //   description: new FormControl(''),
+  //   userId: new FormControl('')
+  // });
+  form: FormGroup;
+  constructor(private fb: FormBuilder, private http: HttpClient, @Inject('BASE_API_URL') private baseUrl: string) {
+    this.form = this.fb.group({
+      title: [''],
+      description: [''],
+      userId: ['']
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.form.value);
+    var title = this.form.get('title').value;
+    var description = this.form.get('description').value;
+    var userId = this.form.get('userId').value;
+    console.log(title)
+    console.log(description)
+    console.log(userId)
+
+    var formData: any = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("userId", userId);
+
+    this.http.post('api/homeprofile', formData).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
+
+  }
 }
