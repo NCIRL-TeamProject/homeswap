9f4194a0434abb951753e2bdc002d7d5e7844f15
Maxi <herreramaxi@gmail.com>
2021-03-27 01:14:01 +0000

    managing cors manually

--- gmapsKeyEndpoint.js
+++ gmapsKeyEndpoint.js
@@ -18,7 +18,18 @@ exports.initialize = (app) => {
     }
 
     const appForGmapsKeyEndpoint = express();
-    appForGmapsKeyEndpoint.use(cors());
+    // appForGmapsKeyEndpoint.use(cors());
+
+
+    appForGmapsKeyEndpoint.use(function (req, res, next) { //allow cross origin requests
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Skip-Interceptor");
+        res.header("Access-Control-Allow-Credentials", true);
+        next();
+    });
+
+
     appForGmapsKeyEndpoint.get('/getKey', function (req, res) {
         res.send({ key: process.env.GMAPS_API_KEY });
     });
