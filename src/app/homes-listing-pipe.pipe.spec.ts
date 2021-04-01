af225c979677982bdcb4a75ef6c4398fd1ffa230
Maxi <herreramaxi@gmail.com>
2021-04-01 01:44:04 +0100

    adding simple filter with pipe

---
+++ src/app/homes-listing-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HomesListingPipePipe } from './homes-listing-pipe.pipe';
+
+describe('HomesListingPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new HomesListingPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
