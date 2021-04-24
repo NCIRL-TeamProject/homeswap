dabbd86fdf75ba2a0a180244fc6181b6f63dbf0f
Maxi <herreramaxi@gmail.com>
2021-04-24 11:43:14 +0100

    Fixing google maps issue and warning on angular build.

--- src/app/services/geocode.service.ts
+++ src/app/services/geocode.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
-declare var google: any;
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,20 +11,15 @@ export class GeocodeService {
 
   geocodeAddress(location: string): Observable<any> {
     if (!this.geocoder) {
-      console.log('Init geocoder');
       this.geocoder = new google.maps.Geocoder();
     }
 
     return new Observable((observer) => {
-      console.log('Start geocoding');
       this.geocoder.geocode({ address: location, 'componentRestrictions': { 'country': 'IE' } }, (results, status) => {
         if (status === 'OK') {
-          console.log('Geocoding complete for location: ' + location);
           const lat = results[0].geometry.location.lat();
           const lng = results[0].geometry.location.lng();
 
-          console.log('lat: ' + lat);
-          console.log('lng: ' + lng);
           observer.next({
             lat: lat,
             lng: lng
